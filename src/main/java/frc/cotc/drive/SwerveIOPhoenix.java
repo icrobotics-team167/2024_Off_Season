35a5b8999f02516c84fc7ae8b85f5dc53058b431
ハイドラント <thecomputer314@gmail.com>
2024-10-09 12:05:29 -0500

    Do some work on SwerveIOPhoenix
    - Tada

--- src/main/java/frc/cotc/drive/SwerveIOPhoenix.java
+++ src/main/java/frc/cotc/drive/SwerveIOPhoenix.java
@@ -7,6 +7,38 @@
 
 package frc.cotc.drive;
 
-import com.ctre.phoenix6.controls.*;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.sim.CANcoderSimState;
+import com.ctre.phoenix6.sim.TalonFXSimState;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 
-public class SwerveIOPhoenix implements SwerveIO {}
+public class SwerveIOPhoenix implements SwerveIO {
+  private static class Module {
+    private final TalonFX driveMotor;
+    private final TalonFX steerMotor;
+    private final CANcoder encoder;
+
+    public Module(int id) {
+      driveMotor = new TalonFX(id * 3, "Croppenheimer");
+      steerMotor = new TalonFX(id * 3 + 1, "Croppenheimer");
+      encoder = new CANcoder(id * 3 + 2, "Croppenheimer");
+    }
+
+    public void run(SwerveModuleState state, double forceFeedforward) {}
+  }
+
+  private static class SimThread extends Thread {
+    private static class SimModule {
+      private final TalonFXSimState driveMotorSim;
+      private final TalonFXSimState steerMotorSim;
+      private final CANcoderSimState encoderSim;
+
+      public SimModule(Module module) {
+        driveMotorSim = module.driveMotor.getSimState();
+        steerMotorSim = module.steerMotor.getSimState();
+        encoderSim = module.encoder.getSimState();
+      }
+    }
+  }
+}
