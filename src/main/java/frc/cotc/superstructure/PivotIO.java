543894d3835c7af50332a1903ee5281dc27d24e6
ハイドラント <thecomputer314@gmail.com>
2024-11-21 18:18:22 -0600

    Some disgusting Java-brained stuff
    - Tada

--- src/main/java/frc/cotc/superstructure/PivotIO.java
+++ src/main/java/frc/cotc/superstructure/PivotIO.java
@@ -8,41 +8,48 @@
 package frc.cotc.superstructure;
 
 import frc.cotc.util.MotorCurrentDraws;
+import org.littletonrobotics.junction.AutoLog;
 import org.littletonrobotics.junction.LogTable;
 import org.littletonrobotics.junction.inputs.LoggableInputs;
 
 public interface PivotIO {
   class PivotIOInputs implements LoggableInputs {
-    double angleRad;
-    double velRadPerSec;
-
     double leftAngleRad;
+    double leftVelRadPerSec;
     double rightAngleRad;
+    double rightVelRadPerSec;
 
     MotorCurrentDraws leftMotorCurrents = new MotorCurrentDraws(0, 0);
     MotorCurrentDraws rightMotorCurrents = new MotorCurrentDraws(0, 0);
 
     @Override
     public void toLog(LogTable table) {
-      table.put("angleRad", angleRad);
-      table.put("velRadPerSec", velRadPerSec);
       table.put("leftAngleRad", leftAngleRad);
+      table.put("leftVelRadPerSec", leftVelRadPerSec);
       table.put("rightAngleRad", rightAngleRad);
+      table.put("rightVelRadPerSec", rightVelRadPerSec);
       table.put("leftMotorCurrents", MotorCurrentDraws.struct, leftMotorCurrents);
       table.put("rightMotorCurrents", MotorCurrentDraws.struct, rightMotorCurrents);
     }
 
     @Override
     public void fromLog(LogTable table) {
-      angleRad = table.get("angleRad", 0.0);
-      velRadPerSec = table.get("velRadPerSec", 0.0);
       leftAngleRad = table.get("leftAngleRad", 0.0);
+      leftVelRadPerSec = table.get("leftVelRadPerSec", 0.0);
       rightAngleRad = table.get("rightAngleRad", 0.0);
+      rightVelRadPerSec = table.get("rightVelRadPerSec", 0.0);
       leftMotorCurrents =
           table.get("leftMotorCurrents", MotorCurrentDraws.struct, new MotorCurrentDraws(0, 0));
       rightMotorCurrents =
           table.get("rightMotorCurrents", MotorCurrentDraws.struct, new MotorCurrentDraws(0, 0));
     }
+  }
+
+  @AutoLog
+  class PivotIOConstants {
+    double maxSpeedRadPerSec;
+    double maxAngleRad;
+    double minAngleRad;
   }
 
   default void updateInputs(PivotIOInputs inputs) {}
---
+++ src/main/java/frc/cotc/superstructure/PivotIOSparkFlexReal.java
@@ -0,0 +1,97 @@
+// Copyright (c) 2024 FRC 167
+// https://github.com/icrobotics-team167
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package frc.cotc.superstructure;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import frc.cotc.util.MotorCurrentDraws;
+
+public class PivotIOSparkFlexReal extends PivotIOSparkFlexAbstract {
+  private final SparkFlex leftMotor;
+  private final RelativeEncoder leftEncoder;
+  private final SparkFlex rightMotor;
+  private final RelativeEncoder rightEncoder;
+
+  private final DutyCycleEncoder angleEncoder;
+
+  public PivotIOSparkFlexReal() {
+    // TODO: Configure CAN IDs
+    leftMotor = new SparkFlex(0, SparkLowLevel.MotorType.kBrushless);
+    rightMotor = new SparkFlex(0, SparkLowLevel.MotorType.kBrushless);
+
+    var config = new SparkFlexConfig();
+    config.idleMode(SparkBaseConfig.IdleMode.kBrake);
+    config.smartCurrentLimit(40);
+    config.voltageCompensation(12);
+    config.signals.appliedOutputPeriodMs(10);
+    config.encoder.positionConversionFactor(2 * Math.PI / gearRatio);
+    config.encoder.velocityConversionFactor(2 * Math.PI / (gearRatio * 60));
+
+    leftEncoder = leftMotor.getEncoder();
+    rightEncoder = rightMotor.getEncoder();
+
+    leftMotor.configure(
+        new SparkFlexConfig(),
+        SparkBase.ResetMode.kResetSafeParameters,
+        SparkBase.PersistMode.kNoPersistParameters);
+    rightMotor.configure(
+        new SparkFlexConfig(),
+        SparkBase.ResetMode.kResetSafeParameters,
+        SparkBase.PersistMode.kNoPersistParameters);
+
+    angleEncoder = new DutyCycleEncoder(0);
+    leftEncoder.setPosition(getAbsoluteAngleRad());
+    rightEncoder.setPosition(getAbsoluteAngleRad());
+  }
+
+  @Override
+  double getLeftAngleRad() {
+    return leftEncoder.getPosition();
+  }
+
+  @Override
+  double getLeftVelRadPerSec() {
+    return leftEncoder.getVelocity();
+  }
+
+  @Override
+  double getRightAngleRad() {
+    return rightEncoder.getPosition();
+  }
+
+  @Override
+  double getRightVelRadPerSec() {
+    return rightEncoder.getVelocity();
+  }
+
+  @Override
+  void runVoltage(double leftVolts, double rightVolts) {
+    leftMotor.setVoltage(leftVolts);
+    rightMotor.setVoltage(rightVolts);
+  }
+
+  @Override
+  void updateCurrents(MotorCurrentDraws left, MotorCurrentDraws right) {
+    double leftStatorAmps = leftMotor.getOutputCurrent();
+    double leftDutyCycle = leftMotor.getAppliedOutput();
+    left.mutate(leftStatorAmps, leftStatorAmps * Math.abs(leftDutyCycle));
+    double rightStatorAmps = leftMotor.getOutputCurrent();
+    double rightDutyCycle = leftMotor.getAppliedOutput();
+    left.mutate(rightStatorAmps, rightStatorAmps * Math.abs(rightDutyCycle));
+  }
+
+  private double getAbsoluteAngleRad() {
+    return Units.rotationsToRadians(angleEncoder.get() - (193.5 / 360));
+  }
+}
